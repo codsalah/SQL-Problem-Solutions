a8b697a0090100fb6631cee0eccb15df303d9b43
codsalah <salahhamada63@gmail.com>
2024-08-03 01:35:44 +0300

    3/8/24

---
+++ Leetcode problems/Easy/1398.customers-who-bought-products-a-and-b-but-not-c.sql
@@ -0,0 +1,5 @@
+SELECT customer_id, customer_name
+FROM Customers c
+WHERE customer_id IN(SELECT customer_id FROM Orders WHERE product_name = 'A')
+AND customer_id IN(SELECT customer_id FROM Orders WHERE product_name = 'B')
+AND customer_id NOT IN(SELECT customer_id FROM Orders WHERE product_name = 'C')
--- Leetcode problems/Easy/1873. Calculate Special Bonus.sql
+++ Leetcode problems/Easy/1873. Calculate Special Bonus.sql
@@ -1,4 +1,18 @@
+-- MySQL
 select employee_id, 
     (if(employee_id % 2 != 0 and name not like 'M%', salary, 0)) as bonus
     from Employees
     order by employee_id;
+
+
+-- MSSQL
+SELECT employee_id, 
+    (CASE
+        WHEN employee_id % 2 <> 0 
+            and 
+        name not like 'M%' THEN salary  
+    ELSE 0
+    END) as bonus
+from Employees
+order by employee_id;
+
