f3623b6b1c38f02dfa2cce88ae6c30d616f0f921
codsalah <salahhamada63@gmail.com>
2024-08-16 08:39:13 +0300

    16/8/24

--- Leetcode problems/Easy/1965. Employees With Missing Information.sql
+++ Leetcode problems/Easy/1965. Employees With Missing Information.sql
@@ -4,3 +4,14 @@ UNION
 select employee_id from Salaries 
 where employee_id not in (select employee_id from Employees) 
 order by employee_id 
+
+-----
+
+SELECT T.employee_id
+FROM  
+  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+   UNION 
+   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+AS T
+WHERE T.salary IS NULL OR T.name IS NULL
+ORDER BY employee_id;
