3774b3fc6235fb5e21104043d389c718fdba5181
codsalah <salahhamada63@gmail.com>
2025-08-11 04:08:18 +0300

    11/08/25

---
+++ SQL-Practice/Medium/Q3.sql
@@ -0,0 +1,4 @@
+-- Show patient_id and first_name from patients
+-- where their first_name start and ends with 's' and is at least 6 characters long.
+
+select patient_id, first_name from patients where first_name like 'S%____s';
